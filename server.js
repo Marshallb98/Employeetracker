4a8dd5a23c240fc670d87b315e7f70012a4f2252
Marshall Bertschy <basemarsh@gmail.com>
2021-01-19 21:17:32 -0600

    added view roles and department functions

--- server.js
+++ server.js
@@ -17,15 +17,6 @@ connection.connect(function (err) {
     start()
 })
 
-function employeeData() {
-    connection.query("SELECT employee.first_name, employee.last_name, role.title, department.name, role.salary FROM employee INNER JOIN role ON (employee.role_id = role.id) LEFT JOIN department ON (role.department_id = department.id)", function (err, res) {
-        if (err) throw err;
-        console.log("employee data!")
-        console.table(res);
-        start();
-    })
-}
-
 function start() {
     inquirer
         .prompt([{
@@ -33,12 +24,12 @@ function start() {
             type: "list",
             message: "What would you like to do?",
             choices: ["View All Employees",
-                "View All Employees By Department",
-                "View All Employees By Manager",
+                "View all Departments",
+                "View all Roles",
                 "Add Employee",
                 "Remove Employee",
                 "Update Employee Role",
-                "Update Employee Manager",
+                "Update Employee Department",
                 "Done"
             ]
 
@@ -49,12 +40,12 @@ function start() {
                     employeeData();
                     break;
 
-                case "View All Employees By Department":
+                case "View all Departments":
                     employeeDep();
                     break;
 
-                case "View All Employees By Manager":
-                    employeeMgr();
+                case "View all Roles":
+                    employeeRole();
                     break;
 
                 case "Add Employee":
@@ -69,8 +60,8 @@ function start() {
                     employeeUpdateRole();
                     break;
 
-                case "Update Employee Manager":
-                    employeeUpdateMgr();
+                case "Update Employee Department":
+                    employeeUpdateDep();
                     break;
 
                 case "Done":
@@ -78,4 +69,31 @@ function start() {
                     break;
             }
         })
+}
+
+function employeeData() {
+    connection.query("SELECT employee.first_name, employee.last_name, role.title, department.name, role.salary FROM employee INNER JOIN role ON (employee.role_id = role.id) LEFT JOIN department ON (role.department_id = department.id)", function (err, res) {
+        if (err) throw err;
+        console.log("employee data!")
+        console.table(res);
+        start();
+    })
+}
+
+function employeeDep() {
+    connection.query("SELECT * FROM employee_db.department", function (err, res) {
+        if (err) throw err;
+        console.log("employee departments!")
+        console.table(res);
+        start();
+    })
+}
+
+function employeeRole() {
+    connection.query("SELECT * FROM employee_db.role", function (err, res) {
+        if (err) throw err;
+        console.log("employee roles!")
+        console.table(res);
+        start();
+    })
 }
