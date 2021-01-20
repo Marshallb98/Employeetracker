f080846615a09680276ba383a8a6f3785f222876
Marshall Bertschy <basemarsh@gmail.com>
2021-01-19 19:36:31 -0600

    added start function

--- server.js
+++ server.js
@@ -13,4 +13,59 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
+    start()
 })
+
+function start() {
+    inquirer
+        .prompt([{
+            name: "start",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View All Employees",
+                "View All Employees By Department",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee",
+                "Update Employee Role",
+                "Update Employee Manager",
+                "Done"
+            ]
+
+        }])
+        .then(function(answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                    employeeData();
+                    break;
+
+                case "View All Employees By Department":
+                    employeeDep();
+                    break;
+
+                case "View All Employees By Manager":
+                    employeeMgr();
+                    break;
+
+                case "Add Employee":
+                    employeeAdd();
+                    break;
+
+                case "Remove Employee":
+                    employeeRemove();
+                    break;
+
+                case "Update Employee Role":
+                    employeeUpdateRole();
+                    break;
+
+                case "Update Employee Manager":
+                    employeeUpdateMgr();
+                    break;
+
+                case "Done":
+                    connection.end();
+                    break;
+            }
+        })
+}
